09f1f3a6c06b7a93631b69cd1a8988f3d8a75ece
1000ch <orz1000ch@gmail.com>
2013-10-01 00:15:04 +0900

    define Class

--- src/lazyload.js
+++ src/lazyload.js
@@ -1,58 +1,61 @@
 (function(window, document) {
 
-  var loadOffset = 100;
-  var targetAttribute = 'data-src';
-  var imgArray = [];
   var win = window;
   var doc = document;
   var docElem = document.documentElement;
-  var windowHeight = 0;
-
-  if (docElem.clientHeight >= 0) {
-    windowHeight = docElem.clientHeight;
-  } else if (doc.body && doc.body.clientHeight >= 0) {
-    windowHeight = doc.body.clientHeight;
-  } else if (win.innerHeight >= 0) {
-    windowHeight = win.innerHeight;
-  }
 
   /**
-   * return that img is visible or not
-   * @param img
-   * @returns {boolean}
+   * Lazyload Class
+   * @constructor Lazyload
    */
-  function isShown(img) {
-    return !!(docElem.compareDocumentPosition(img) & 16) && (img.getBoundingClientRect().top < windowHeight + loadOffset);
-  }
+  function Lazyload() {
+    this.imgArray = [];
+    this.windowHeight = 0;
+    if (docElem.clientHeight >= 0) {
+      this.windowHeight = docElem.clientHeight;
+    } else if (doc.body && doc.body.clientHeight >= 0) {
+      this.windowHeight = doc.body.clientHeight;
+    } else if (win.innerHeight >= 0) {
+      this.windowHeight = win.innerHeight;
+    }
 
-  /**
-   * show images
-   * @returns {boolean}
-   */
-  function showImages() {
-    var completed = true;
-    var img;
-    for(var i = 0, len = imgArray.length;i < len;i++) {
-      img = imgArray[i];
-      if(img) {
-        completed = false;
-        if(isShown(img)) {
-          img.src = img.getAttribute(targetAttribute);
-          img.removeAttribute(targetAttribute);
-          imgArray[i] = null;
+    //listen DOMContentLoaded
+    doc.addEventListener('DOMContentLoaded', (function() {
+      //get image elements
+      var img, imgs = doc.getElementsByTagName('img');
+      for(var i = 0, len = imgs.length;i < len;i++) {
+        img = imgs[i];
+        if(img.hasAttribute(Lazyload.targetAttribute) && this.imgArray.indexOf(img) === -1) {
+          this.imgArray.push(img);
         }
       }
-    }
-    return completed;
+      this.showImages();
+    }).bind(this));
+
+    //scroll event handler which is throttled
+    var scrollEventHandler = Lazyload.throttle((function(e) {
+      if(this.showImages()) {
+        //if all images are loaded.
+        //release memory
+        this.imgArray = [];
+        //unbind scroll event
+        win.removeEventListener('scroll', scrollEventHandler);
+      }
+    }).bind(this), 300);
+
+    //listen scroll event
+    win.addEventListener('scroll', scrollEventHandler);
   }
 
+  Lazyload.loadOffset = 100;
+  Lazyload.targetAttribute = 'data-src';
   /**
    * throttle
    * @param fn
    * @param delay
    * @returns {Function}
    */
-  function throttle(fn, delay) {
+  Lazyload.throttle = function(fn, delay) {
     var timer = null;
     return function() {
       var context = this, args = arguments;
@@ -61,34 +64,38 @@
         fn.apply(context, args);
       }, delay);
     };
-  }
+  };
+  /**
+   * return that img is visible or not
+   * @param img
+   * @returns {boolean}
+   */
+  Lazyload.prototype.isShown = function(img) {
+    return !!(docElem.compareDocumentPosition(img) & 16) && (img.getBoundingClientRect().top < this.windowHeight + Lazyload.loadOffset);
+  };
 
-  doc.addEventListener('DOMContentLoaded', function() {
-    //get image elements
-    var img, imgs = doc.getElementsByTagName('img');
-    for(var i = 0, len = imgs.length;i < len;i++) {
-      img = imgs[i];
-      if(img.hasAttribute(targetAttribute) && imgArray.indexOf(img) === -1) {
-        imgArray.push(img);
+  /**
+   * show images
+   * @returns {boolean}
+   */
+  Lazyload.prototype.showImages = function() {
+    var completed = true;
+    var img;
+    for(var i = 0, len = this.imgArray.length;i < len;i++) {
+      img = this.imgArray[i];
+      if(img) {
+        completed = false;
+        if(this.isShown(img)) {
+          img.src = img.getAttribute(Lazyload.targetAttribute);
+          img.removeAttribute(Lazyload.targetAttribute);
+          this.imgArray[i] = null;
+        }
       }
     }
-  });
+    return completed;
+  };
 
-  //scroll event handler which is throttled
-  var scrollEventHandler = throttle(function(e) {
-    if(showImages()) {
-      //if all images are loaded.
-      //release memory
-      imgArray = [];
-      //unbind scroll event
-      win.removeEventListener('scroll', scrollEventHandler);
-    }
-  }, 300);
-
-  win.addEventListener('load', function(e) {
-    showImages();
-  });
-
-  win.addEventListener('scroll', scrollEventHandler);
+  //export
+  win.Lazyload = Lazyload;
 
 })(window, document);
