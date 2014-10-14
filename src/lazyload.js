027c42f0b806a555d61cb6318e32f59a08e52f58
1000ch <shogo.sensui@gmail.com>
2014-10-14 10:29:14 +0900

    remove srcset feature

--- src/lazyload.js
+++ src/lazyload.js
@@ -8,7 +8,6 @@
 
   // cache to local
   var TARGET_SRC = 'data-src';
-  var TARGET_SRCSET = 'data-srcset';
   var FALLBACK_IMAGE = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAEElEQVR42gEFAPr/AP///wAI/AL+Sr4t6gAAAABJRU5ErkJggg==';
 
   /**
@@ -61,105 +60,6 @@
 
     // images which got in half of display forward will be loaded
     return windowHeight * 1.5;
-  }
-
-  /**
-   * Emulate srcset
-   * @see http://www.w3.org/html/wg/drafts/srcset/w3c-srcset/
-   * @see http://dev.w3.org/csswg/css-values-3/#resolution-value
-   * @param {String} srcset
-   * pear-mobile.jpeg 720w, pear-tablet.jpeg 1280w, pear-desktop.jpeg 1x
-   */
-  function _resolveSrcset (src, srcset) {
-
-    // for detection
-    var dpr = window.devicePixelRatio;
-    var width = window.innerWidth;
-
-    // include 5.14 default candidate
-    var candidate;
-    var candidates = [{
-      url: src,
-      w: Infinity,
-      x: 1
-    }];
-
-    // found resolution
-    var resolution = {
-      w: Infinity,
-      x: 1
-    };
-
-    var re = /[^\,\s](.+?)\s([0-9]+)([wx])(\s([0-9]+)([wx]))?/g;
-    var match;
-    while ((match = re.exec(srcset)) !== null) {
-
-      var url = match[1];
-      var number1 = match[2] | 0;
-      var unit1 = match[3];
-      var number2 = match[5] | 0;
-      var unit2 = match[6];
-
-      // find optimal width and dpr
-      if (unit1 === 'w' && number1) {
-        if (width < number1 && number1 <= resolution.w) {
-          resolution.w = number1;
-
-          // define as candidate
-          candidate = {
-            url: url,
-            w: number1
-          };
-
-          if (unit2 === 'x' && number2) {
-            if (dpr <= number2 && (dpr > resolution.x && number2 < resolution.x)) {
-              resolution.x = number2;
-              candidate.x = resolution.x;
-            }
-          }
-
-          candidates.push(candidate);
-        }
-      } else if (unit1 === 'x' && number1) {
-        if (dpr <= number1 && (dpr > resolution.x && number1 < resolution.x)) {
-          resolution.x = number1;
-
-          // define as candidate
-          candidate = {
-            url: url,
-            x: number1
-          };
-
-          if (unit2 === 'w' && number2) {
-            if (width < number2 && number2 <= resolution.w) {
-              resolution.w = number2;
-              candidate.w = resolution.w;
-            }
-          }
-
-          candidates.push(candidate);
-        }
-      }
-    }
-
-    // return matched url with resolution
-    var c, i, l;
-    for (i = 0, l = candidates.length;i < l;i++) {
-      c = candidates[i];
-      if (resolution.w === c.w && resolution.x === c.x) {
-        return c.url;
-      }
-    }
-    for (i = 0, l = candidates.length;i < l;i++) {
-      c = candidates[i];
-      if (resolution.x === c.x) {
-        return c.url;
-      } else if (resolution.w === c.w) {
-        return c.url;
-      }
-    }
-
-    return src;
   }
 
   /**
@@ -237,13 +137,7 @@
           img.src = FALLBACK_IMAGE;
         };
 
-        var src = img.getAttribute(TARGET_SRC);
-        var srcset = img.getAttribute(TARGET_SRCSET);
-        if (srcset) {
-          img.src = _resolveSrcset(src, srcset);
-        } else {
-          img.src = src;
-        }
+        img.src = img.getAttribute(TARGET_SRC);
       }
     });
   };
@@ -296,13 +190,7 @@
           img.src = FALLBACK_IMAGE;
         };
 
-        var src = img.getAttribute(TARGET_SRC);
-        var srcset = img.getAttribute(TARGET_SRCSET);
-        if (srcset) {
-          img.src = _resolveSrcset(src, srcset);
-        } else {
-          img.src = src;
-        }
+        img.src = img.getAttribute(TARGET_SRC);
       }
     });
 
